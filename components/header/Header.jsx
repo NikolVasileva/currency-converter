13738f203797d9ab2861208c56b673b752f4ea9e
NikolVasileva <nikol.vasileva9@gmail.com>
2026-01-16 23:12:16 +0200

    relocate styles into separated file

--- components/header/Header.jsx
+++ components/header/Header.jsx
@@ -1,32 +1,11 @@
-import { StyleSheet, Text, View } from "react-native";
+import { Text, View } from "react-native";
+import { styles } from "../../styles/header.js"
 
 export default function Header() {
     return (
-        <View style={styles.header}>
-            <Text style={styles.headline}>Currency Converter</Text>
-            <Text style={styles.subHeadline}>Lorem Ipsum is simply dummy text of the printing.</Text>
+        <View style={[styles.header]}>
+            <Text style={[styles.headline]}>Currency Converter</Text>
+            <Text style={[styles.subHeadline]}>Lorem Ipsum is simply dummy text of the printing.</Text>
         </View>
     )
 }
-
-const styles = StyleSheet.create({
-    header: {
-        textAlign: "center",
-        alignItems: "center",
-        paddingHorizontal: 69,
-        paddingVertical: 50,
-        gap: 5,
-    },
-    
-    headline: {
-        fontSize: 25,
-        fontWeight: "bold",
-        color: "#1F2261",
-    },
-
-    subHeadline: {
-        textAlign: "center",
-        fontSize: 16,
-        color: "#808080",
-    }
-})
---
+++ styles/header.js
@@ -0,0 +1,23 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+    header: {
+        textAlign: "center",
+        alignItems: "center",
+        paddingHorizontal: 69,
+        paddingVertical: 50,
+        gap: 5,
+    },
+    
+    headline: {
+        fontSize: 25,
+        fontWeight: "bold",
+        color: "#1F2261",
+    },
+
+    subHeadline: {
+        textAlign: "center",
+        fontSize: 16,
+        color: "#808080",
+    }
+})
