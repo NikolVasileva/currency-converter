df0a4e62cb45502b5bb11c1ba3d775542023ac30
NikolVasileva <nikol.vasileva9@gmail.com>
2026-01-18 15:33:44 +0200

    Update footer style

--- components/footer/Footer.jsx
+++ components/footer/Footer.jsx
@@ -2,14 +2,22 @@ import { Text, View } from "react-native";
 
 export default function Footer() {
     return(
-        <View>
+        <View style={{
+           paddingHorizontal: 28,
+           paddingVertical: 15,
+           }}>
             <Text style={{
                     fontSize: 15,
                     color: "#989898",
                     paddingBottom: 5,
                 }}>Indicative Exchange Rate
             </Text>
-            <Text>1 EUR = 1.96 BGN</Text>
+            <Text style={{
+                fontSize: 18,
+                fontWeight: "bold",
+               }}>
+                1 EUR = 1.96 BGN
+            </Text>
         </View>
     )
 }
